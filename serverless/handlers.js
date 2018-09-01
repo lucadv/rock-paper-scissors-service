85803929fa92d367fd71a25f6c35a129fed0a9bb
Luca Di Vincenzo <luca.divincenzo@piksel.com>
2018-09-02 00:01:47 +0200

    refactoring serverless handler to callback with a proper response

--- serverless/handlers.js
+++ serverless/handlers.js
@@ -2,6 +2,10 @@ const PickShape = require('../lib/pickShape');
 
 module.exports = {
   pickShape: (event, context, callback) => {
-    callback(null, PickShape());
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(PickShape())
+    };
+    callback(null, response);
   }
 };
--- test/serverless/pickShapeHandler.js
+++ test/serverless/pickShapeHandler.js
@@ -14,19 +14,29 @@ describe('Serverless tests', () => {
 
   describe('pickShape function', () => {
 
+    it('should return a 200 OK statusCode', () => LambdaTester(PickShapeHandler)
+      .expectResult((res) => {
+        expect(res.statusCode).to.equal(200);
+      }));
+    
+    it('should return a body', () => LambdaTester(PickShapeHandler)
+      .expectResult((res) => {
+        expect(res.body).to.exist();
+      }));
+
     it('should return a document with a picked property', () => LambdaTester(PickShapeHandler)
       .expectResult((res) => {
-        expect(res.picked).to.exist();
+        expect(JSON.parse(res.body).picked).to.exist();
       }));
 
     it('should return only one propery', () => LambdaTester(PickShapeHandler)
       .expectResult((res) => {
-        expect(Object.keys(res)).to.have.length(1);
+        expect(Object.keys(JSON.parse(res.body))).to.have.length(1);
       }));
 
     it('should have a picked property which is one of rock, paper or scissors', () => LambdaTester(PickShapeHandler)
       .expectResult((res) => {
-        expect(['rock', 'paper', 'scissors']).to.include(res.picked);
+        expect(['rock', 'paper', 'scissors']).to.include(JSON.parse(res.body).picked);
       }));
   });
 });
