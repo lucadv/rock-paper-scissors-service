ac66c4b67dcd7146a56edd697b8b05bd0a5bc724
Luca Di Vincenzo <luca.divincenzo@piksel.com>
2018-09-04 23:07:37 +0200

    adding description to endpoints

--- server/routes.js
+++ server/routes.js
@@ -5,11 +5,17 @@ const ErrorWrapper = require('../lib/errorWrapper');
 module.exports = [{
   method: 'GET',
   path: '/',
+  options: {
+    description: 'Root endpoint that automatically redirects to /docs endpoint.'
+  },
   handler: (request, h) => h.redirect('/docs')
 }, {
   method: 'GET',
   path: '/play',
   options: {
+    description: 'This endpoint allow to play the game Rock, Paper, Scissors. Use the query string'
+      + 'parameter withPlayerMove to pass a shape and play your move. Game results will be passed'
+      + 'back to response in JSON format.',
     validate: {
       query: Validation.query,
       failAction: async (request, h, err) => {
