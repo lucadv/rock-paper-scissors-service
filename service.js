2e44f08aa0b7725e28b031e96531db102a62126b
Luca Di Vincenzo <luca.divincenzo@piksel.com>
2018-09-04 23:10:24 +0200

    no need to expose a create server endpoint

--- service.js
+++ service.js
@@ -4,24 +4,19 @@ const Inert = require('inert');
 const Lout = require('lout');
 const Routes = require('./server/routes');
 
-const createServer = () => {
+const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT || 5000,
     host: process.env.HOST || '0.0.0.0',
   });
-
+  // adding routes
   server.route(Routes);
-  return server;
-};
-
-const init = async () => {
-  const server = createServer();
+  // registering plugins for documentation endpoint
   await server.register([Vision, Inert, Lout]);
   await server.start();
   console.log(`Server running at: ${server.info.uri}`);
 };
 
 module.exports = {
-  createServer,
   init
 };
